f85d808c9461a7ef28fc765465cc0f3f7ba97bfc
Ilejk <adrianskibaa@gmail.com>
2023-03-20 22:03:52 +0100

    20.03.2023-22:04

---
+++ android/app/src/main/kotlin/com/example/pull_and_bear_replica/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.pull_and_bear_replica
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/src/app/app.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+// ignore: must_be_immutable
+class MyApp extends StatefulWidget {
+  MyApp._internal();
+  int appState = 0;
+  static final MyApp instance = MyApp._internal();
+
+  factory MyApp() => instance;
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      onGenerateRoute: RouteGenerator.getRoute,
+      initialRoute: Routes.mainStartingPage,
+      theme: getApplicationTheme(),
+    );
+  }
+}
